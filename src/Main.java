e6309f40dbcc5365addd71764f0a1750d96d404e
41377671810 <lucaspereiraoliveira10@hotmail.com>
2026-06-20 08:49:31 -0300

    --link notion

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+    //link notion: https://oracle-jfun.notion.site/Java-Foundations-Atividades-34c1811581598000a8fbf72a75e356ac?source=copy_link
 //        AtividadesListas.atividade5();
 
     }
